c7d33302603a9ec8998377c303f5eed15f9bf1eb
Tim Grob <tim.grob@swissonline.ch>
2025-02-02 23:06:18 +0100

    make endpoints plural

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = FastAPI(title="Uniswap Transactions API")
 
 
-@app.get("/api/v1/txn/{txn_hash_key}")
+@app.get("/api/v1/txns/{txn_hash_key}")
 def fetch_transaction(txn_hash_key: str):
     if txn_hash_key not in []:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Transaction hash key not found")
@@ -17,20 +17,20 @@ def fetch_transaction(txn_hash_key: str):
     # TODO: implement transaction get logic for fetching a single transaction
     return {}
 
-@app.post("/api/v1/txn/")
+@app.get("/api/v1/txns/")
+def fetch_all_transactions():
+
+    # TODO: implement transaction get logic for fetching all stored transactions
+    return {}
+
+
+@app.post("/api/v1/txns/")
 def create_transaction(txn: Transaction):
     # TODO: Check if transaction already exisists
     if txn.id in []: 
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Transaction already exisits")
     
     # TODO: implement transaction logic to create new transaction
-    return {}
-
-
-@app.get("/api/v1/txns/")
-def fetch_all_transactions():
-
-    # TODO: implement transaction get logic for fetching all stored transactions
     return {}
 
 
@@ -41,7 +41,7 @@ def fetch_transactions_in_time_period(time_period: TimePeriod):
     return {}
 
 
-@app.get("/api/v1/txn-fee/{txn_hash_key}")
+@app.get("/api/v1/txn-fees/{txn_hash_key}")
 def fetch_transaction_fee(txn_hash_key: str):
     txn = fetch_transaction(txn_hash_key)
     
@@ -49,7 +49,7 @@ def fetch_transaction_fee(txn_hash_key: str):
     return {}
 
 
-@app.post("/api/v1/txn-fee/")
+@app.post("/api/v1/txn-fees/")
 def fetch_transaction_fee_in_time_period(time_period: TimePeriod):
     
     # TODO: implement transaction post logic to fetch transactions in time period
