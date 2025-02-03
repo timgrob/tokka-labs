d9684c98e604dc3c33f96bfc4ef2f57f9f3f8a0e
Tim Grob <tim.grob@swissonline.ch>
2025-02-03 20:36:05 +0100

    binance response is a nested list

--- controllers/fees.py
+++ controllers/fees.py
@@ -13,7 +13,7 @@ async def fetch_ethusdt_price_at_timestamp(timestamp: int) -> float:
     async with httpx.AsyncClient() as client:
         response = await client.get(url=BINANCE_API_URL, params=params)
         data = response.json()
-        price = float(data[4])  # Price of ETH/USDT
+        price = float(data[0][4])  # Closing price of ETH/USDT
         return price
 
 
