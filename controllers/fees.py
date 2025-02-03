0962c594cb26ff6a55652096b6e2429639e56766
Tim Grob <tim.grob@swissonline.ch>
2025-02-03 18:55:53 +0100

    implement calculation for gas fees

--- controllers/fees.py
+++ controllers/fees.py
@@ -1,5 +1,11 @@
 from models.transaction import Transaction
 
 
-def calculate_txn_fees(txns: list[Transaction]) -> float:
-    return 0.0
+WAI = 1E-18     # Wai unit in ETH 
+GWAI = 1E-9     # Gwai unit in ETH
+
+
+def calculate_total_gas_fees(txns: list[Transaction]) -> float:
+    # CALCULATION FORMULA: total gas = units of gas used * (base fee + priority fee)
+    # returns gas fees in ETH
+    return sum([txn.gas_used * txn.gas_price * WAI for txn in txns])
