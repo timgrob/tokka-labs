03116d8bf9d4fa412859d68feb63ee737e41eaec
Tim Grob <tim.grob@swissonline.ch>
2025-02-05 16:04:26 +0100

    write test for Rest endpoings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from sqlmodel import SQLModel, Session, create_engine, select
 from config import DATABASE_URL
 from models.time_period import TimePeriod
@@ -14,15 +14,20 @@ app = FastAPI(title="Uniswap Transactions API", description="Tokka Labs Coding C
 # Database
 engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
 
+# Dependency
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 @app.on_event("startup")
 async def startup_event():
     SQLModel.metadata.create_all(engine)
 
     txns = await fetch_all_transactions()
-    unique_txns = set(txns)
+
     with Session(engine) as session:
-        session.add_all(unique_txns)
+        session.add_all(txns)
         session.commit()
 
     # TODO: Continuously fetch new transactions
@@ -39,12 +44,27 @@ def index():
     return {"status": "server is running"}
 
 
+@app.post("/api/v1/txn/")
+async def create_transaction(txn: Transaction, session: Session = Depends(get_session)) -> Transaction:
+    session.add(txn)
+    session.commit()
+    session.refresh()
+
+    return {
+        "status": status.HTTP_200_OK,
+        "message": "OK",
+        "result": txn
+        }
+
+
 @app.get("/api/v1/txns")
-async def all_transactions():
-    with Session(engine) as session:
+async def all_transactions(session: Session = Depends(get_session)):
+    try:
         query = select(Transaction)
         results = session.exec(query)
         txns = results.all()
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Transactions could not be fetched from database")
 
     return {
         "status": status.HTTP_200_OK,
@@ -55,29 +75,36 @@ async def all_transactions():
 
 
 @app.get("/api/v1/txns/{txn_hash}")
-async def transactions_by_hash(txn_hash: str):
-    try: 
-        with Session(engine) as session:
-            query = select(Transaction).where(Transaction.hash == txn_hash)
-            results = session.exec(query)
-            txn = results.one()
+async def transactions_by_hash(txn_hash: str, session: Session = Depends(get_session)):
+    try:
+        query = select(Transaction).where(Transaction.hash == txn_hash)
+        results = session.exec(query)
+        txns = results.all()
     except:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Multiple transactions found with hash={txn_hash}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Transactions could not be fetched from database")
+
+    if not txns:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Transaction not found")
 
     return {
         "status": status.HTTP_200_OK,
         "message": "OK",
-        "number_of_transactions": 1,
-        "result": txn
+        "number_of_transactions": len(txns),
+        "result": txns
         }
 
 
 @app.post("/api/v1/txns")
-async def transactions_in_time_period(time_period: TimePeriod):
-    with Session(engine) as session:
+async def transactions_in_time_span(time_period: TimePeriod, session: Session = Depends(get_session)):
+    try: 
         query = select(Transaction).where(Transaction.time_stamp >= time_period.start_timestamp, Transaction.time_stamp <= time_period.end_timestamp)
         results = session.exec(query)
         txns = results.all()
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Transactions could not be fetched from database")
+
+    if not txns:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No transactions found in time span")
 
     return {
         "status": status.HTTP_200_OK,
@@ -87,15 +114,22 @@ async def transactions_in_time_period(time_period: TimePeriod):
     }
 
 
-@app.get("/api/v1/txns-fees/{txn_hash}")
-async def transaction_fees_by_hash(txn_hash: str):
+@app.get("/api/v1/txn-fees/{txn_hash}")
+async def transaction_fees_by_hash(txn_hash: str, session: Session = Depends(get_session)):
     try: 
-        with Session(engine) as session:
-            query = select(Transaction).where(Transaction.hash == txn_hash)
-            results = session.exec(query)
-            txn = results.one()
+        query = select(Transaction).where(Transaction.hash == txn_hash)
+        results = session.exec(query)
+        txns = results.all()
     except:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Transaction not found with hash={txn_hash}")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Multiple transactions found")
+
+    if not txns:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No transaction found")
+
+    # Some transactions have the same hash and thus multiple transactions can be returned. 
+    # Yet, the transaction fees are the same. Thus, take the first found transaction only
+    if len(txns) > 0:
+        txn = txns[0]
 
     try:
         price_ethusdt = await fetch_ethusdt_price_at_timestamp(txn.time_stamp)
@@ -110,18 +144,24 @@ async def transaction_fees_by_hash(txn_hash: str):
     }
 
 
-@app.post("/api/v1/txns-fees")
-async def transaction_fees_in_time_period(time_period: TimePeriod):
-    with Session(engine) as session:
-        query = select(Transaction).where(Transaction.time_stamp >= time_period.start_timestamp, Transaction.time_stamp <= time_period.end_timestamp)
-        results = session.exec(query)
-        txns = results.all()
+@app.post("/api/v1/txn-fees")
+async def transaction_fees_in_time_period(time_period: TimePeriod, session: Session = Depends(get_session)):
+    query = select(Transaction).where(Transaction.time_stamp >= time_period.start_timestamp, Transaction.time_stamp <= time_period.end_timestamp)
+    results = session.exec(query)
+    txns = results.all()
 
-    if len(txns) == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No txns found in time span")
+    if not txns:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No transactions found in time span")
 
-    timestamps = [txn.time_stamp for txn in txns]
-    gas_fees = [txn.gas_fee for txn in txns]
+    # Remove duplicate transactions with the same hash key
+    if len(txns) > 0:
+        txns_unique: set[Transaction] = set(txns)
+
+    print(f"======== length: {len(txns)}")
+    print(f"======== length: {len(txns_unique)}")
+
+    timestamps = [txn.time_stamp for txn in txns_unique]
+    gas_fees = [txn.gas_fee for txn in txns_unique]
     prices_ethusdt = await fetch_ethusdt_prices_at_timestamps(timestamps)
     gas = np.array(gas_fees)
     prices = np.array(prices_ethusdt)
--- tests/test_main.py
+++ tests/test_main.py
@@ -11,6 +11,80 @@ def test_index():
     assert response.json() == {"status": "server is running"}
 
 
-# def test_fetch_all_transaction():
-#     response = client.get("/api/v1/txns")
-#     assert response.status_code == 200
+def test_all_transactions():
+    response = client.get("/api/v1/txns")
+    result = response.json()["result"]
+
+    assert response.status_code == 200
+    assert isinstance(result, list)
+
+
+def test_transactions_by_hash():
+    txn_hash = "0x125e0b641d4a4b08806bf52c0c6757648c9963bcda8681e4f996f09e00d4c2cc"
+    response = client.get(f"/api/v1/txns/{txn_hash}")
+    result = response.json()["result"]
+
+    assert response.status_code == 200
+    assert  isinstance(result, list)
+    assert len(result) == 2
+
+
+def test_transactions_by_hash_not_found():
+    txn_hash = "0x01"
+    response = client.get(f"/api/v1/txns/{txn_hash}")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail":"Transaction not found"}
+
+
+def test_transactions_in_time_span():
+    time_span_json = {"start_timestamp": 1620250931, "end_timestamp": 1620254651}
+    response = client.post("/api/v1/txns", json=time_span_json)
+    result = response.json()["result"]
+
+    assert response.status_code == 200
+    assert isinstance(result, list)
+    assert len(result) == 8
+
+
+def test_transactions_in_time_period_faulty_time_span():
+    time_span_json = {"start_timestamp": 1620252901, "end_timestamp": 1620250931}
+    response = client.post("/api/v1/txns", json=time_span_json)
+
+    assert response.status_code == 422
+    assert response.json() == {"detail": [{"loc": ["body", "end_timestamp"], "msg": "end_timestamp must be after start_timestamp", "type": "value_error"}]
+}
+
+def test_transaction_fees_by_hash():
+    txn_hash = "0x125e0b641d4a4b08806bf52c0c6757648c9963bcda8681e4f996f09e00d4c2cc"
+    response = client.get(f"/api/v1/txn-fees/{txn_hash}")
+    fees = response.json()["gas_fee_usdt"]
+
+    assert response.status_code == 200
+    assert fees == 1140.197909488
+
+
+def test_transaction_fees_by_hash_not_found():
+    txn_hash = "0x00"
+    response = client.get(f"/api/v1/txn-fees/{txn_hash}")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail":"No transaction found"}
+
+
+def test_transaction_fees_in_time_period():
+    time_span_json = {"start_timestamp": 1620250931, "end_timestamp": 1620252901}
+    response = client.post("/api/v1/txn-fees", json=time_span_json)
+    fees = response.json()["gas_fee_usdt"]
+
+    assert response.status_code == 200
+    assert fees == 1199.3995683250241
+
+
+def test_transaction_fees_in_time_period_faulty_time_span():
+    time_span_json = {"start_timestamp": 1620252901, "end_timestamp": 1620250931}
+    response = client.post("/api/v1/txn-fees", json=time_span_json)
+
+    assert response.status_code == 422
+    assert response.json() == {"detail": [{"loc": ["body", "end_timestamp"], "msg": "end_timestamp must be after start_timestamp", "type": "value_error"}]
+}
